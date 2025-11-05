1bbd32d720bc4798d62cd1d8b70d15cc8ab15c32
SabrinaOtto <ms00944499@gmail.com>
2025-11-05 13:53:31 +0500

    react library issue fix

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-export default {
-  base: "/portfolio/",
-};
+export default defineConfig({
+  base: "/portfolio/",   // <-- MUST match your GitHub repo name EXACTLY (lowercase)
+  plugins: [react()],
+});
